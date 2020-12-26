ff6552caf6f3a50b01d1557826ebe39cd9a66889
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-12-26 17:27:49 -0500

    refactor(build): improve static/dynamic detection
    Now:
    1. Does a sanity check to make sure we're building for Linux.
    2. Checks for the LIBSCMP_STATIC and LIBSECCOMP_STATIC environmental
       variables, and honors the first one found.
    3. Tries to dynamically link by default if Alpine's custom musl target
       (which dynamically links) is detected.

--- build.rs
+++ build.rs
@@ -1,7 +1,45 @@
 fn main() {
-    match std::env::var("CARGO_CFG_TARGET_ENV").unwrap().as_str() {
-        "" | "gnu" => println!("cargo:rustc-link-lib=dylib=seccomp"),
-        "musl" => println!("cargo:rustc-link-lib=static=seccomp"),
-        target_env => panic!("Unsupported target environment {:?}", target_env),
+    // Sanity check
+    assert_eq!(
+        std::env::var("CARGO_CFG_TARGET_OS").unwrap(),
+        "linux",
+        "This crate is Linux-only",
+    );
+
+    if link_static() {
+        println!("cargo:rustc-link-lib=static=seccomp");
+    } else {
+        println!("cargo:rustc-link-lib=dylib=seccomp");
+    }
+
+    println!("cargo:rerun-if-env-changed=LIBSECCOMP_STATIC");
+    println!("cargo:rerun-if-env-changed=LIBSCMP_STATIC");
+}
+
+fn link_static() -> bool {
+    // Check LIBSCMP_STATIC or LIBSECCOMP_STATIC
+    if let Ok(link_type) =
+        std::env::var("LIBSCMP_STATIC").or_else(|_| std::env::var("LIBSECCOMP_STATIC"))
+    {
+        return match link_type.as_str() {
+            "0" | "false" => false,
+            _ => true,
+        };
+    }
+
+    let target_vendor = std::env::var("CARGO_CFG_TARGET_VENDOR").unwrap();
+    let target_env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
+
+    match (target_vendor.as_str(), target_env.as_str()) {
+        // When linking against glibc, or Alpine's built-in musl target, default to dynamic linking.
+        (_, "") | (_, "gnu") | ("alpine", "musl") => false,
+
+        // Against other musl setups, default to static linking.
+        (_, "musl") => true,
+
+        _ => panic!(
+            "Unsupported target vendor/environment {:?}/{:?}",
+            target_vendor, target_env
+        ),
     }
 }
