72fcb082f961817817df7cd560fd79949b680fa1
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-06-05 15:51:21 -0400

    refactor: improve static linking detection in build.rs

--- build.rs
+++ build.rs
@@ -24,19 +24,7 @@ fn link_static() -> bool {
         return !matches!(link_type.as_str(), "0" | "false");
     }
 
-    let target_vendor = std::env::var("CARGO_CFG_TARGET_VENDOR").unwrap();
-    let target_env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
-
-    match (target_vendor.as_str(), target_env.as_str()) {
-        // When linking against glibc, or Alpine's built-in musl target, default to dynamic linking.
-        (_, "") | (_, "gnu") | ("alpine", "musl") => false,
-
-        // Against other musl setups, default to static linking.
-        (_, "musl") => true,
-
-        _ => panic!(
-            "Unsupported target vendor/environment {:?}/{:?}",
-            target_vendor, target_env
-        ),
-    }
+    std::env::var("CARGO_CFG_TARGET_FEATURE")
+        .unwrap_or_default()
+        .contains("crt-static")
 }
